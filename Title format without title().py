1ec98948164b74f5c8a17d013a2227e40e1a2cbe
AryamannSrivastava <153055812+ARYAMANN7279@users.noreply.github.com>
2024-02-20 22:24:48 +0530

    Delete Title format without title().py

--- Title format without title().py
+++
@@ -1,7 +0,0 @@
-def format_name(a,b):
-  a=a[0].upper()+a[1:].lower()
-  b=b[0].upper()+b[1:].lower()
-  return(a+" "+b)
-f_name=input("enter your first name ")
-l_name=input("enter your last name ")
-print(f"Your name in title format is {format_name(f_name,l_name)}")     
